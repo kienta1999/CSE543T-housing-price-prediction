483b2ba85d874b873b92cdc0f08967026ab177e7
Kien Ta <talekien1710@gmail.com>
2021-11-18 01:28:08 -0600

    final_data.csv generated

--- data_preparation2.py
+++ data_preparation2.py
@@ -11,7 +11,7 @@ from time import sleep
 path_to_browser = "./chromedriver.exe"
 ser = Service(path_to_browser)
 op = webdriver.ChromeOptions()
-op.add_argument('headless')
+op.add_argument("headless")
 driver = webdriver.Chrome(service=ser, options=op)
 driver.maximize_window()
 
@@ -24,11 +24,11 @@ index = 0
 # cooling,heating,has_pool,parking_size,price\n')
 # data_file.close()
 
-all_href = pd.read_csv(f'data/property_href/{index}.csv')['href']
+all_href = pd.read_csv(f"data/property_href/{index}.csv")["href"]
 for i, href in enumerate(all_href):
     # for href in ['https://www.redfin.com/CA/San-Francisco/401-Harrison-St-94105/unit-46B/home/144064341', \
     #             'https://www.redfin.com/NY/Fort-Plain/38-Clinton-Ave-13339/home/92339866']:
-    if i <= 13872:
+    if i <= 16425:
         continue
     data_file = open(f"./data/housing_data_raw/{index}.csv", "a")
     progress_file = open("progress2.txt", "a")
@@ -38,12 +38,12 @@ for i, href in enumerate(all_href):
     if not data:
         continue
     [price, beds, bath, area] = [c.text for c in data]
-    price = price.replace(',', '')
-    area = area.replace(',', '')
-    stress_address = driver.find_elements(
-        By.CSS_SELECTOR, ".street-address")[0].text[:-1]
-    city_and_state = driver.find_elements(
-        By.CSS_SELECTOR, ".dp-subtext")[0].text
+    price = price.replace(",", "")
+    area = area.replace(",", "")
+    stress_address = driver.find_elements(By.CSS_SELECTOR, ".street-address")[0].text[
+        :-1
+    ]
+    city_and_state = driver.find_elements(By.CSS_SELECTOR, ".dp-subtext")[0].text
 
     # home_facts = driver.find_elements(By.CSS_SELECTOR, ".keyDetailsList")[0] \
     #                    .find_elements(By.CSS_SELECTOR, "div.keyDetail")
@@ -56,73 +56,103 @@ for i, href in enumerate(all_href):
     year_built = county = None
     if basic_infor:
         basic_infor = basic_infor[0]
-        basic_infor_label = [e.text for e in basic_infor.find_elements(
-            By.CSS_SELECTOR, "span.table-label")]
-        basic_infor_value = [e.text for e in basic_infor.find_elements(
-            By.CSS_SELECTOR, "div.table-value")]
-        year_built = basic_infor_value[basic_infor_label.index(
-            'Year Built')] if 'Year Built' in basic_infor_label else None
-        county = basic_infor_value[basic_infor_label.index(
-            'County')] if 'County' in basic_infor_label else None
+        basic_infor_label = [
+            e.text
+            for e in basic_infor.find_elements(By.CSS_SELECTOR, "span.table-label")
+        ]
+        basic_infor_value = [
+            e.text
+            for e in basic_infor.find_elements(By.CSS_SELECTOR, "div.table-value")
+        ]
+        year_built = (
+            basic_infor_value[basic_infor_label.index("Year Built")]
+            if "Year Built" in basic_infor_label
+            else None
+        )
+        county = (
+            basic_infor_value[basic_infor_label.index("County")]
+            if "County" in basic_infor_label
+            else None
+        )
     # lot_size = basic_infor_value[basic_infor_label.index('Lot Size')] if 'Lot Size' in basic_infor_label else None
     # lot_size = lot_size.replace(',', '')
     scores = driver.find_elements(By.CSS_SELECTOR, ".walk-score")
     walk_score = transit_score = bike_score = None
     if scores:
         scores = scores[0].find_elements(By.CSS_SELECTOR, ".score")
-        trademark = [score.find_elements(
-            By.CSS_SELECTOR, ".walkscore-trademark")[0].text[:-1] for score in scores]
-        score_value = [score.find_elements(By.CSS_SELECTOR, "div.percentage > span.value")[
-            0].text for score in scores]
-        walk_score = score_value[trademark.index(
-            'Walk Score')] if 'Walk Score' in trademark else None
-        transit_score = score_value[trademark.index(
-            'Transit Score')] if 'Transit Score' in trademark else None
-        bike_score = score_value[trademark.index(
-            'Bike Score')] if 'Bike Score' in trademark else None
+        trademark = [
+            score.find_elements(By.CSS_SELECTOR, ".walkscore-trademark")[0].text[:-1]
+            for score in scores
+        ]
+        score_value = [
+            score.find_elements(By.CSS_SELECTOR, "div.percentage > span.value")[0].text
+            for score in scores
+        ]
+        walk_score = (
+            score_value[trademark.index("Walk Score")]
+            if "Walk Score" in trademark
+            else None
+        )
+        transit_score = (
+            score_value[trademark.index("Transit Score")]
+            if "Transit Score" in trademark
+            else None
+        )
+        bike_score = (
+            score_value[trademark.index("Bike Score")]
+            if "Bike Score" in trademark
+            else None
+        )
 
-    property_details_container = driver.find_elements(
-        By.CSS_SELECTOR, "div.super-group-content > div.amenity-group") or []
+    property_details_container = (
+        driver.find_elements(
+            By.CSS_SELECTOR, "div.super-group-content > div.amenity-group"
+        )
+        or []
+    )
     pool_text = heating_cooling_text = parking_garage_text = lot_text = []
     for pc in property_details_container:
         header = pc.find_elements(By.TAG_NAME, "h3")[0].text
-        if header == 'Pool Information':
+        if header == "Pool Information":
             pool_text = [e.text for e in pc.find_elements(By.TAG_NAME, "li")]
-        if header == 'Heating & Cooling':
-            heating_cooling_text = [
-                e.text for e in pc.find_elements(By.TAG_NAME, "li")]
-        if header == 'Parking & Garage Information':
-            parking_garage_text = [
-                e.text for e in pc.find_elements(By.TAG_NAME, "li")]
-        if header == 'Lot Information':
+        if header == "Heating & Cooling":
+            heating_cooling_text = [e.text for e in pc.find_elements(By.TAG_NAME, "li")]
+        if header == "Parking & Garage Information":
+            parking_garage_text = [e.text for e in pc.find_elements(By.TAG_NAME, "li")]
+        if header == "Lot Information":
             lot_text = [e.text for e in pc.find_elements(By.TAG_NAME, "li")]
 
     lot_size = cooling = heating = has_pool = parking_size = None
     for lt in lot_text:
-        if lt.startswith('Lot Size Acres: '):
-            lot_size = lt.split('Lot Size Acres: ')[1].replace(',', '|')
+        if lt.startswith("Lot Size Acres: "):
+            lot_size = lt.split("Lot Size Acres: ")[1].replace(",", "|")
     for hc in heating_cooling_text:
-        if hc.startswith('Cooling: '):
-            cooling = hc.split('Cooling: ')[1].replace(',', '|')
-        if hc.startswith('Heating: '):
-            heating = hc.split('Heating: ')[1].replace(',', '|')
+        if hc.startswith("Cooling: "):
+            cooling = hc.split("Cooling: ")[1].replace(",", "|")
+        if hc.startswith("Heating: "):
+            heating = hc.split("Heating: ")[1].replace(",", "|")
     for p in pool_text:
-        if p.startswith('Has Private Pool: '):
-            has_pool = p.split('Has Private Pool: ')[1].replace(',', '|')
+        if p.startswith("Has Private Pool: "):
+            has_pool = p.split("Has Private Pool: ")[1].replace(",", "|")
     for pg in parking_garage_text:
-        if pg.startswith('Parking Total: '):
-            parking_size = pg.split('Parking Total: ')[1].replace(',', '|')
+        if pg.startswith("Parking Total: "):
+            parking_size = pg.split("Parking Total: ")[1].replace(",", "|")
 
-    photo_elements = driver.find_elements(
-        By.CSS_SELECTOR, 'div.InlinePhotoPreview')
+    photo_elements = driver.find_elements(By.CSS_SELECTOR, "div.InlinePhotoPreview")
     photo_elements = photo_elements[0] if photo_elements else []
-    img_href = " ".join([img.get_attribute('src')
-                        for img in photo_elements.find_elements(By.TAG_NAME, "img")])
+    img_href = " ".join(
+        [
+            img.get_attribute("src")
+            for img in photo_elements.find_elements(By.TAG_NAME, "img")
+        ]
+    )
 
     # lot_size, cooling, heating, has_pool, parking_size
-    data_file.write(f'{href},{stress_address},{city_and_state},{county},{img_href},{beds},{bath},{area},{year_built},{lot_size},{walk_score},{transit_score},{bike_score},{lot_size},{cooling},{heating},{has_pool},{parking_size},{price}\n')
-    progress_file.write(f'crawling done for property {i} - {href}\n')
+    data_file.write(
+        f"{href},{stress_address},{city_and_state},{county},{img_href},{beds},{bath},{area},{year_built},{lot_size},{walk_score},{transit_score},{bike_score},{lot_size},{cooling},{heating},{has_pool},{parking_size},{price}\n"
+    )
+    progress_file.write(f"crawling done for property {i} - {href}\n")
     data_file.close()
-print(f'index {index} done!')
-print(f'index {index} done!')
-print(f'index {index} done!')
+print(f"index {index} done!")
+print(f"index {index} done!")
+print(f"index {index} done!")
--- data_preparation3.py
+++ data_preparation3.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from us_state_to_abbrev import us_state_to_abbrev
 
 source_raw = "data/housing_data_raw/0.csv"
 housing_price = pd.read_csv(source_raw, encoding="cp1252", error_bad_lines=False)
@@ -25,13 +26,13 @@ mean_year = int(
         housing_price[
             (housing_price["year_built"] != "—")
             & (housing_price["year_built"] != "None")
-        ]["year_built"].astype(np.float)
+        ]["year_built"].astype(float)
     )
 )
 housing_price["year_built"] = (
     housing_price["year_built"]
     .map(lambda year: year if year != "—" and year != "None" else mean_year)
-    .astype(np.int)
+    .astype(int)
 )
 # Lot size
 housing_price["lot_size"] = housing_price["lot_size"].map(
@@ -58,8 +59,157 @@ housing_price["heating"] = housing_price["heating"].map(
 housing_price["has_pool"] = housing_price["has_pool"].map(
     lambda score: 1 if score == "Yes" else 0
 )
+# extract state and zip
+housing_price["state"] = housing_price["state_and_zip"].map(
+    lambda x: x.split(" ")[1] if len(x.split(" ")) == 3 else None
+)
+housing_price["zip"] = housing_price["state_and_zip"].map(
+    lambda x: x.split(" ")[2] if len(x.split(" ")) == 3 else None
+)
+
+# drop house with no county
+housing_price.dropna(inplace=True)
+housing_price = housing_price[housing_price["county"] != "None"]
+
+# reset index
+housing_price = housing_price.reset_index(drop=True)
 
 # append columns from other files
 
+# Crime rate
+def get_crime_rate(county, state):
+    county_name = county + ", " + state
+    crime_rate_entry = crime_rate[
+        crime_rate["county_name"].str.lower() == county_name.lower()
+    ]
+    return (
+        crime_rate_entry.iloc[0]["crime_rate_per_100000"]
+        if crime_rate_entry.shape[0]
+        else None
+    )
+
+
+crime_rate = pd.read_csv("data/crime_data_w_population_and_crime_rate.csv")[
+    ["county_name", "crime_rate_per_100000"]
+]
+house_crime_rate = []
+for i in range(housing_price.shape[0]):
+    house = housing_price.iloc[i]
+    house_crime_rate.append(get_crime_rate(house["county"], house["state"]))
+housing_price["crime_rate_per_100000"] = house_crime_rate
+
+# Minimum wage
+minimum_wage_data = pd.read_csv("./data/minimum_wage_data.csv", encoding="cp1252")
+minimum_wage_data = minimum_wage_data[minimum_wage_data["Year"] == 2020][
+    ["Year", "State", "State.Minimum.Wage"]
+]
+minimum_wage_data["State"] = minimum_wage_data["State"].map(
+    lambda s: us_state_to_abbrev[s]
+)
+
+
+def get_minimum_wage(state):
+    minimum_wage_entry = minimum_wage_data[minimum_wage_data["State"] == state]
+    return (
+        minimum_wage_entry.iloc[0]["State.Minimum.Wage"]
+        if minimum_wage_entry.shape[0]
+        else 7.25
+    )
+
+
+housing_price["minimum_wage"] = housing_price["state"].map(get_minimum_wage)
+
+# education - college completion rate
+education_completing_college_data = pd.read_excel(
+    "data/education_completing_college.xlsx"
+)
+education_completing_college_data = education_completing_college_data[
+    ["Name", "2015-2019"]
+]
+
+
+def get_education(county, state):
+    if len(county.split(" ")) < 1:
+        return None
+    county = " ".join(county.split(" ")[:-1])
+    county_name = county + ", " + state
+    education_completing_college_entry = education_completing_college_data[
+        education_completing_college_data["Name"].str.lower() == county_name.lower()
+    ]
+    return (
+        education_completing_college_entry.iloc[0]["2015-2019"]
+        if education_completing_college_entry.shape[0]
+        else None
+    )
+
+
+college_completion = []
+for i in range(housing_price.shape[0]):
+    house = housing_price.iloc[i]
+    college_completion.append(get_education(house["county"], house["state"]))
+housing_price["college_completion"] = college_completion
+
+# unemployment rate and median household income
+unemployment_rate_data = pd.read_excel("data/unemployment_rate.xlsx")[
+    ["Name", 2020, "Median Household Income (2019)"]
+]
+
+
+def get_unemployment_rate(county, state):
+    if county == "San Francisco County":
+        county = "San Francisco County/city"
+    county_name = county + ", " + state
+    unemployment_rate_entry = unemployment_rate_data[
+        unemployment_rate_data["Name"].str.lower() == county_name.lower()
+    ]
+    return (
+        unemployment_rate_entry.iloc[0][2020]
+        if unemployment_rate_entry.shape[0]
+        else None,
+        unemployment_rate_entry.iloc[0]["Median Household Income (2019)"]
+        if unemployment_rate_entry.shape[0]
+        else None,
+    )
+
+
+unemployment_rate = []
+median_household_income = []
+for i in range(housing_price.shape[0]):
+    house = housing_price.iloc[i]
+    u, m = get_unemployment_rate(house["county"], house["state"])
+    unemployment_rate.append(u)
+    median_household_income.append(m)
+housing_price["unemployment_rate"] = unemployment_rate
+housing_price["median_household_income"] = median_household_income
+
 # export data
+housing_price.dropna(inplace=True)
+housing_price = housing_price[
+    [
+        "href",
+        "stress_address",
+        "city",
+        "state",
+        "zip",
+        "county",
+        "img_href",
+        "beds",
+        "bath",
+        "area",
+        "year_built",
+        "lot_size",
+        "walk_score",
+        "transit_score",
+        "bike_score",
+        "cooling",
+        "heating",
+        "has_pool",
+        "crime_rate_per_100000",
+        "minimum_wage",
+        "college_completion",
+        "unemployment_rate",
+        "median_household_income",
+        "price",
+    ]
+]
 housing_price.to_csv("data/final_data.csv", index=False)
