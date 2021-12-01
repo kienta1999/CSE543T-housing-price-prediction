eee39b959aa4045f0f849c51f05a98c7f3a3f7eb
Kien Ta <talekien1710@gmail.com>
2021-11-30 23:57:05 -0600

    minior random state fix

--- sklearn_model/cross_validation.py
+++ sklearn_model/cross_validation.py
@@ -9,17 +9,17 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import VotingRegressor
 from sklearn.model_selection import cross_val_score
 
-# print('-------------------------------------------------------')
-# print('KNN Regressor')
-# k_values = np.arange(1, 31)
-# accuracies = np.zeros(k_values.shape[0])
-# for i, k in enumerate(k_values):
-#     knn = KNeighborsRegressor(n_neighbors=k)
-#     scores = cross_val_score(knn, X_train_scaled, y_train, cv=10)
-#     accuracies[i] = np.mean(scores)
+print('-------------------------------------------------------')
+print('KNN Regressor')
+k_values = np.arange(1, 31)
+accuracies = np.zeros(k_values.shape[0])
+for i, k in enumerate(k_values):
+    knn = KNeighborsRegressor(n_neighbors=k)
+    scores = cross_val_score(knn, X_train_scaled, y_train, cv=10)
+    accuracies[i] = np.mean(scores)
 
-# max_k = k_values[np.argmax(accuracies)]
-# print(f"The best k for KNN Regressor is {max_k}")
+max_k = k_values[np.argmax(accuracies)]
+print(f"The best k for KNN Regressor is {max_k}")
 
 print('-------------------------------------------------------')
 print('Random Forest Regressor')
@@ -29,7 +29,7 @@ comb = [(x, y) for x in max_features for y in n_estimators]
 accuracies = np.zeros(len(comb))
 for i, c in enumerate(comb):
     max_feature, n_estimator = c
-    rf = RandomForestRegressor(max_features=max_feature, n_estimators=n_estimator, random_state=11)
+    rf = RandomForestRegressor(max_features=max_feature, n_estimators=n_estimator, random_state=4)
     scores = cross_val_score(rf, X_train_scaled, y_train, cv=10)
     accuracies[i] = np.mean(scores)
     print(c, accuracies[i])
